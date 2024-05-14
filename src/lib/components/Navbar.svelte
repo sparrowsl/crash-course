485a6eed6d3cb4e2b4b4ad17623a253710be886b
sparrowsl <benthorpe.dev@gmail.com>
2024-05-14 10:04:08 +0000

    add github icon instead of the text on navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,9 +1,18 @@
+<script>
+	import "iconify-icon";
+</script>
+
 <nav class="flex justify-between items-center container mx-auto min-h-20">
 	<a href="/" class="block font-bold text-2xl text-accent">CC</a>
 
 	<ul class="flex items-center gap-5 text-sm font-fira-sans">
 		<li><a href="/">Home</a></li>
 		<li><a href="/languages">Languages</a></li>
-		<li><a href="https://github.com/sparrowsl/crash-course">GitHub</a></li>
+		<li>
+			<a href="https://github.com/sparrowsl/crash-course">
+				<iconify-icon icon="simple-icons:github" class="text-2xl">
+				</iconify-icon>
+			</a>
+		</li>
 	</ul>
 </nav>
