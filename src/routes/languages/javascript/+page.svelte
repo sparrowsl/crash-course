f761bdfdd5d7a10b274fe23388c7f226868c3542
sparrowsl <benthorpe.dev@gmail.com>
2024-05-14 09:54:34 +0000

    update biomejs

--- src/routes/languages/javascript/+page.svelte
+++ src/routes/languages/javascript/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-// biome-ignore lint/correctness/noUnusedImports: <explanation>
-import Overview from "./overview.md";
+	// biome-ignore lint/correctness/noUnusedImports: <explanation>
+	import Overview from "./overview.md";
 </script>
 
 <article class="prose">
