83d333cada46c6c56d6191b7c627aebc5f40ad03
sparrowsl <benthorpe.dev@gmail.com>
2024-12-05 12:48:29 +0000

    switch to adapter-auto

--- src/routes/languages/go/[slug]/+page.js
+++ src/routes/languages/go/[slug]/+page.js
@@ -1,13 +1,13 @@
 import { marked } from "marked";
 
-const modules = import.meta.glob("./*.md", {
-	eager: true,
-	query: "?raw",
-	import: "default",
-});
-
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
+	const modules = import.meta.glob("./*.md", {
+		eager: true,
+		query: "?raw",
+		import: "default",
+	});
+
 	const topics = Object.keys(modules).map((file_path) => {
 		const file = modules[file_path];
 		const parsed = marked.parse(file);
@@ -17,10 +17,11 @@ export async function load({ params }) {
 			content: parsed,
 		};
 	});
-
+	// console.log(topic);
 	const found = topics.find((topic) => topic.slug === params.slug);
 
 	return { content: found?.content };
+	return { content: "" };
 
 	// const post = (await import(`./${params.topic}.md`));
 	// console.log((await modules[`./${params.topic}.md`]()).default);
